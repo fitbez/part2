e150cd716e8f2b2fc83430032040fe037f869b5c
fitsum belayneh <fitsu_alt@yahoo.com>
2019-10-14 12:39:38 -0400

    2.9*: The Phonebook Step4

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,9 +2,15 @@ import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" }
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filterName, setFilterName] = useState("");
 
   const handleNameChange = e => {
     // console.log(e.target.value);
@@ -14,7 +20,15 @@ const App = () => {
     // console.log(e.target.value);
     setNewNumber(e.target.value);
   };
+  const handleFilterName = e => {
+    // console.log(e.target.value);
+    setFilterName(e.target.value);
+  };
 
+  const filteredName = filterName.toUpperCase();
+  const nameToShow = persons.filter(
+    person => person.name.toUpperCase().indexOf(filteredName) > -1
+  );
   const addPerson = event => {
     event.preventDefault();
     const personObject = {
@@ -26,12 +40,20 @@ const App = () => {
     setNewName(" ");
     setNewNumber(" ");
   };
-  console.log(newName);
-  console.log(persons);
+  // console.log(newName);
+  // console.log(nameToShow);
+  // console.log(filteredName);
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with{" "}
+        <input value={filterName} onChange={handleFilterName} />
+      </div>
+      <div>
+        <h2>add a new</h2>
+      </div>
       <form>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -48,10 +70,10 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => {
+        {nameToShow.map(person => {
           return person.name !== newName ? (
             <li key={person.name}>
-              {person.name}
+              {person.name} {""}
               {person.number}
             </li>
           ) : (
